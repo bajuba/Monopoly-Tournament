a9d1b5f877ee2003fe8c507827729ebc7e535109
Thomas Eichinger <ThomasEichy@live.com>
2021-02-20 11:38:15 -0600

    Setup basic menu with for Player & Game Classes with basic menu examples of for each class

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Monopoly
 {
@@ -7,10 +8,34 @@ namespace Monopoly
         static void Main(string[] args)
         {
             Menu menuInst = new Menu();
+            List<Tournament> Tournaments = new List<Tournament>();
 
             menuInst.Intro();
-            
         }
+    }
+
+    class Tournament
+    {
+        public int ID;
+        public string Name;
+        public string Date;
+        public string Address;
+        public string City;
+        public string State;
+        public int MaxPlayers;
+
+        // TODO Andy, this is your nested game class to code
+        // - Fill in the info for needed stored for each game
+        class Game 
+        {
+
+        }
+    }
+
+    // TODO Grace, this is your player class to code
+    class Players
+    {
+
     }
 
     class Menu
@@ -33,26 +58,52 @@ namespace Monopoly
             }
         }
 
+        // TODO Andy, this is our feature menu here, we will code this menu while Grace codes the player one
+        // - focus on making the features and methods that modify game class data and keep track of the games of each tourney
+        // - I'll try and focus on the overall data for the tournaments themselves and stuff that modifies them
+
         public void TournamentOptions()
         {
             Console.WriteLine("Hello! Please enter the section of the application" +
-            " you would like to use. \nYour choices are below. \n\nAdd Tournament | Remove Tournament\n");
+            " you would like to use. \nYour choices are below. \n\nAdd Tournament | Remove Tournament | Remove Tournament |" +
+            " Add Game\n");
 
             string tournMenuSelect = Console.ReadLine().ToLower();
 
             if (tournMenuSelect == "add tournament")
             {
-                //AddTournament();
+                AddTournament();
             }
             else if (tournMenuSelect == "remove tournament")
             {
-                //RemoveTournament();
+                RemoveTournament();
+            }
+            else if (tournMenuSelect == "add game")
+            {
+                AddGame();
             }
         }
 
+        // TODO Grace, this is your menu to code
+
         public void PlayersOptions()
         {
-            Console.WriteLine("\nPlayers Section");
+            Console.WriteLine("\nPlayer Options\n");
+        }
+
+        public void AddTournament()
+        {
+            Console.WriteLine("\nAdd Tournament");         
+        }
+
+        public void RemoveTournament()
+        {
+            Console.WriteLine("\nRemove Tournament");
+        }
+
+        public void AddGame()
+        {
+            Console.WriteLine("\nAdd Game");
         }
     }
 }
