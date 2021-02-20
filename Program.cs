a7381e293d166351947886bc6bcb1713b6d35de6
Thomas Eichinger <ThomasEichy@live.com>
2021-02-20 09:58:20 -0600

    Setup basic main menu options for branching menus

--- Program.cs
+++ Program.cs
@@ -6,7 +6,54 @@ namespace Monopoly
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Menu menuInst = new Menu();
+
+            menuInst.Intro();
+            
+        }
+    }
+
+    class Menu
+    {
+
+        public void Intro()
+        {
+            Console.WriteLine("Hello! Please enter the section of the application" +
+            " you would like to use. \nYour choices are below. \n\nTournament | Players\n");
+
+            string menuSelect = Console.ReadLine().ToLower();
+            
+            if (menuSelect == "tournament")
+            {
+                TournamentOptions();
+            }
+            else if (menuSelect == "players")
+            {
+                PlayersOptions();
+            }
+        }
+
+        public void TournamentOptions()
+        {
+            Console.WriteLine("Hello! Please enter the section of the application" +
+            " you would like to use. \nYour choices are below. \n\nAdd Tournament | Remove Tournament\n");
+
+            string tournMenuSelect = Console.ReadLine().ToLower();
+
+            if (tournMenuSelect == "add tournament")
+            {
+                //AddTournament();
+            }
+            else if (tournMenuSelect == "remove tournament")
+            {
+                //RemoveTournament();
+            }
+        }
+
+        public void PlayersOptions()
+        {
+            Console.WriteLine("\nPlayers Section");
         }
     }
 }
+
