076713710f036fa687b40d7b32f2a57fe4d8ec8c
Jack Burton <jburton11756@lakelandcollege.edu>
2021-03-01 07:46:23 -0600

    added tourney list class

---
+++ Tournamentslist.cs
@@ -0,0 +1,10 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Monopoly{
+  public static class TournamentList {
+    public static List<Tournament> Tournaments = new List<Tournament>();
+  }
+
+}
